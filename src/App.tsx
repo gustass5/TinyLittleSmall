d89654abb35f6b3c13d921d4859e64281ff3e77a
Gustas <gustass1@gmail.com>
2022-04-08 19:39:49 +0200

    Added three fiber

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Canvas } from '@react-three/fiber';
 function App() {
-	return <h1 className="text-3xl font-bold underline bg-blue-500">Hello world!</h1>;
+	return (
+		<div id="canvas-container">
+			<Canvas>
+				<ambientLight intensity={0.1} />
+				<directionalLight color="red" position={[0, 0, 5]} />
+				<mesh>
+					<boxGeometry />
+					<meshStandardMaterial />
+				</mesh>
+			</Canvas>
+		</div>
+	);
 }
 
 export default App;
